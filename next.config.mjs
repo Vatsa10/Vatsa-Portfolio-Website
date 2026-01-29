4bea265110f7671b53214865ffc5dbd41ce6abda
Vatsa10 <vatsajoshi2@gmail.com>
2026-01-29 22:26:54 +0530

    fix dep error

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,6 @@ const nextConfig = {
     unoptimized: true,
     formats: ['image/avif', 'image/webp'],
   },
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
